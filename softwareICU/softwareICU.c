07f2dfc72942eab2b32dba205abcbaea0e625a94
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-02-02 01:53:00 +0200

    timer2 to trigger icu

--- softwareICU/softwareICU.c
+++ softwareICU/softwareICU.c
@@ -17,14 +17,17 @@ int main(void)
 	Led_Init(LED_1);
 	Led_Init(LED_2);
 	Led_Init(LED_3);
+  timer2Init(T2_NORMAL_MODE,T2_OC2_DIS,T2_PRESCALER_NO,10,0,0,T2_POLLING);
 	gpioPortDirection(GPIOC,HIGH);
 	SwICU_Init(SwICU_EdgeRisiging);
 		while(1)
-    {
+  	{
 		tcnt0_value=timer0Read();
 		SET_BIT(PORTC_DATA,1);
-		_delay_ms(1);
+		timer2Start();
+		timer2DelayUs(1);
 		CLEAR_BIT(PORTC_DATA,1);
 		SwICU_Read(&tcnt0_value);
     }
+
 }
